593c8d5c340b15d5fc8aaccbd940ebed1f532512
Jonathan Soares <jon.sdm.7@gmail.com>
2015-09-25 20:08:26 -0300

    exercicio aula 4 slide 18

---
+++ jsm/aula4/exercicio slide 18.c
@@ -0,0 +1,14 @@
+#include "rims.h"
+void main()
+{
+   while (1) { 
+      if(A >= 7){
+          B0=0;
+          B1=1;
+          }
+      else if(A < 7){
+          B0=1;
+          B1=0;    
+         }
+   }
+}
