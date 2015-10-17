e18f89bff6bbd8c7e2363606ba54873769275d09
Marcus Lucas <marcus_lucas12@outlook.com>
2015-10-17 09:36:30 -0300

    Homework 6

---
+++ mlaaf/aula6/exercicio6.c
@@ -0,0 +1,17 @@
+#include "rims.h"
+
+void main()
+{
+   unsigned char ucI1 = 10;
+   unsigned short usI2 = 2;
+   signed long slI3 = 30;
+   unsigned char bMyBitVar = 1;
+   
+   while (1) {      
+      B = ucI1;
+      B = usI2;
+      B = slI3;
+      B = bMyBitVar;
+   }
+}
+
