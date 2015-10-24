e928b7a5a93bb226995586c7e47c4fb2bc828430
srgfs <saulo_rgfs@msn.com>
2015-10-23 22:32:46 -0300

    Exercício 7

---
+++ srgfs/Aula7/7.4.c
@@ -0,0 +1,17 @@
+#include "rims.h"
+
+void main( ) {
+    
+   while ( 1 ) { 
+
+       if ( A >= 48 ) {
+           B = 0x1;    
+       } else if ( A > 2 && A < 48) {
+           B = 0x2;
+       } else {
+           B = 0x0;
+       }
+
+   }
+   
+}
