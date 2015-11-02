c9898af5a56a6d71d8a45d8668b6d5ab4bf737c1
Rhaissa Santos <rhaissaluiza@gmail.com>
2015-11-02 16:13:35 -0200

    Updating

---
+++ rls/aula6/6/exercicio6.c
@@ -0,0 +1,15 @@
+#include "rims.h"
+
+unsigned char ucI1 = 16;
+unsigned short usI2 = 32;
+signed long slI3 = 64;
+unsigned char bMyBitVar = 128;
+
+void main(){
+   while(1){      
+            B = ucI1;
+            B = usI2;
+            B = slI3;
+            B = bMyBitVar;
+   }
+}
