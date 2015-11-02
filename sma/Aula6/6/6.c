0596a32af45546b7bb04618281849cbc3dd97ef5
sergiomalencar <sergiomalencar@hotmail.com>
2015-11-02 20:26:42 -0300

    Exercicios
    Exercicios

---
+++ sma/Aula6/6/6.c
@@ -0,0 +1,19 @@
+#include "rims.h"
+
+void main( ) {
+   
+   unsigned char ucI1 = 2; 
+   unsigned short usI2 = 1; 
+   signed long slI3 = -1;
+   unsigned char bMyBitVar = 0;
+    
+   while ( 1 ) { 
+       
+      B = ucI1;
+      B = usI2;
+      B = slI3;
+      B = bMyBitVar;
+      
+   }
+   
+}
