e4148408f8c55485ebb3d0cf85cc38ae81c12794
srgfs <saulo_rgfs@msn.com>
2015-11-03 16:24:14 -0300

    Prova1

---
+++ srgfs/Prova1/3.c
@@ -0,0 +1,29 @@
+#include "rims.h"
+
+void main()
+{
+   while (1) { 
+      
+      
+      if(A1== 0 && A0== 0){//Primeiro passo aciondo se A0 = 0 e A1 = 0
+		  B0=1;
+      while(B0=1){
+          if(A1== 0 && A0== 1){// Segundo passo acionado se A0 = 1 e A1 = 0
+			B1=1;
+       while(B1=1){   
+              if(A1== 1 && A0== 0){////Terceiro passo acionado se A0 = 0 e A1 = 1
+				B2=1;
+          while(B2=1){
+                  if(A1== 1 && A0== 1){//Quarto passo acionado se A0 = 1 e A1 = 1
+					B3=1;//Porta Aberta
+                      
+          }
+          }
+       }
+      }
+      }
+      }
+      }
+      
+   }
+}
