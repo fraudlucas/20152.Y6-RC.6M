e81dbeeb7dad35b8280b1c6a236b8c84d8a01440
Anderson <andersonpe@outlook.com.br>
2015-11-13 17:42:27 -0300

    Alteracao da estrutura das pastas e exercicios da aula 8

---
+++ aps/aula8/8/exercicio_8.2_codigo.c
@@ -0,0 +1,11 @@
+#include "rims.h"
+
+void main( ) {
+    
+   while ( 1 ) { 
+       
+      B = ( A & 0x0f ) + ( A >> 4 ) ; 
+      
+   }
+   
+}
---
+++ aps/aula8/8/exercicio_8.4_codigo.c
@@ -0,0 +1,9 @@
+#include "rims.h"
+
+void main( ) {
+    
+   while ( 1 ) { 
+      B = ( A6 && A7 ) + ( ( A0 + A1 + A2 + A3 + A4 + A5 ) << 5 ) ;
+   }
+   
+}
