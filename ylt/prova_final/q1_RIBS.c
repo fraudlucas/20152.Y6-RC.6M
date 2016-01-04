4b0f83d61fcf0c3455dd931a4c729c229d10b466
yujitashiro <yuji.tashiro@gmail.com>
2016-01-04 14:12:34 -0300

    Prova final - Yuji Lima Tashiro

---
+++ ylt/prova_final/q1_RIBS.c
@@ -0,0 +1,70 @@
+/*
+This code was automatically generated using the Riverside-Irvine State machine Builder tool
+Version 2,8 --- 1/4/2016 11:41:4 PST
+*/
+
+#include "rims.h"
+
+signed char erro;
+unsigned char valor_atual, valor_desejado;
+unsigned char checaTemperatura(){
+erro = valor_desejado - valor_atual;
+if(erro <= 0){B = 0;}
+else{B = 1;}
+}
+unsigned char PSEQ1_Clk;
+void TimerISR() {
+   PSEQ1_Clk = 1;
+}
+
+enum PSEQ1_States { PSEQ1_Estado_Inicial, PSEQ1_Controlador_ON_OFF } PSEQ1_State;
+
+TickFct_q1_Yuji() {
+   switch(PSEQ1_State) { // Transitions
+      case -1:
+         PSEQ1_State = PSEQ1_Estado_Inicial;
+         break;
+         case PSEQ1_Estado_Inicial: 
+         if (1) {
+            PSEQ1_State = PSEQ1_Controlador_ON_OFF;
+         }
+         break;
+      case PSEQ1_Controlador_ON_OFF: 
+         if (1) {
+            PSEQ1_State = PSEQ1_Controlador_ON_OFF;
+            checaTemperatura();
+         }
+         break;
+      default:
+         PSEQ1_State = PSEQ1_Estado_Inicial;
+   } // Transitions
+
+   switch(PSEQ1_State) { // State actions
+      case PSEQ1_Estado_Inicial:
+         B = 0;
+         break;
+      case PSEQ1_Controlador_ON_OFF:
+         valor_atual = (A & 0x0F);
+         valor_desejado = ((A & 0xF0) >> 4);
+         break;
+      default: // ADD default behaviour below
+      break;
+   } // State actions
+
+}
+
+int main() {
+
+   const unsigned int periodq1_Yuji = 1000; // 1000 ms default
+   TimerSet(periodq1_Yuji);
+   TimerOn();
+   
+   PSEQ1_State = -1; // Initial state
+   B = 0; // Init outputs
+
+   while(1) {
+      TickFct_q1_Yuji();
+      while(!PSEQ1_Clk);
+      PSEQ1_Clk = 0;
+   } // while (1)
+} // Main
---
+++ ylt/prova_final/q2_RIBS.c
@@ -0,0 +1,83 @@
+/*
+This code was automatically generated using the Riverside-Irvine State machine Builder tool
+Version 2,8 --- 1/4/2016 13:59:43 PST
+*/
+
+#include "rims.h"
+
+unsigned char tempo, H, L;
+unsigned char PSEQ2_Clk;
+void TimerISR() {
+   PSEQ2_Clk = 1;
+}
+
+enum PSEQ2_States { PSEQ2_estado_Inicial, PSEQ2_Parte_H, PSEQ2_Parte_L } PSEQ2_State;
+
+TickFct_q2_Yuji() {
+   switch(PSEQ2_State) { // Transitions
+      case -1:
+         PSEQ2_State = PSEQ2_estado_Inicial;
+         break;
+         case PSEQ2_estado_Inicial: 
+         if (1) {
+            PSEQ2_State = PSEQ2_Parte_H;
+         }
+         break;
+      case PSEQ2_Parte_H: 
+         if (tempo < H) {
+            PSEQ2_State = PSEQ2_Parte_H;
+         }
+         else if (tempo >= H) {
+            PSEQ2_State = PSEQ2_Parte_L;
+            tempo = 0;
+         }
+         break;
+      case PSEQ2_Parte_L: 
+         if (tempo < L) {
+            PSEQ2_State = PSEQ2_Parte_L;
+         }
+         else if (tempo >= L) {
+            PSEQ2_State = PSEQ2_Parte_H;
+            tempo = 0;
+            B = 0;
+         }
+         break;
+      default:
+         PSEQ2_State = PSEQ2_estado_Inicial;
+   } // Transitions
+
+   switch(PSEQ2_State) { // State actions
+      case PSEQ2_estado_Inicial:
+         tempo = 0;
+         H = 8;
+         L = 12;
+         break;
+      case PSEQ2_Parte_H:
+         tempo++;
+         B = 1;
+         break;
+      case PSEQ2_Parte_L:
+         tempo++;
+         B = 2;
+         break;
+      default: // ADD default behaviour below
+      break;
+   } // State actions
+
+}
+
+int main() {
+
+   const unsigned int periodq2_Yuji = 100;
+   TimerSet(periodq2_Yuji);
+   TimerOn();
+   
+   PSEQ2_State = -1; // Initial state
+   B = 0; // Init outputs
+
+   while(1) {
+      TickFct_q2_Yuji();
+      while(!PSEQ2_Clk);
+      PSEQ2_Clk = 0;
+   } // while (1)
+} // Main
